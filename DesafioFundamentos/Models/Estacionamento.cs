73bfd5d3c9454430a6b1f7d7278030378940f71f
Fagner Lisboa <fnlisboa2016@gmail.com>
2024-01-10 21:26:22 -0300

    Desafio de Projeto Decola Tech Avanade 2024

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Text.RegularExpressions;
 
 namespace DesafioFundamentos.Models
@@ -8,7 +7,7 @@ namespace DesafioFundamentos.Models
         private decimal precoInicial = 0;
         private decimal precoPorHora = 0;
         private List<string> veiculos = new List<string>();
-       
+
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
             this.precoInicial = precoInicial;
@@ -17,33 +16,33 @@ namespace DesafioFundamentos.Models
 
         public bool AdicionarVeiculo()
         {
+            //TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // IMPLEMENTADO!
             Console.WriteLine("Digite a placa do veículo para estacionar:");
             string veiculo = Console.ReadLine();
 
-            var estacionar = ValidarPlaca(veiculo);
-            var mensagem = estacionar ? "Veículo estacionado!" : "Placa inválida, digite novamente:";
-            
-            Console.WriteLine($"{mensagem}");
-            Console.ReadKey();
+            var placa = ValidarPlaca(veiculo);
+            var mensagem = placa ? "Veículo estacionado!" : "Placa inválida, digite novamente:";
 
-            if(estacionar) 
+            Console.WriteLine($"{mensagem}");
+
+            if (placa)
             {
-              veiculos.Add(veiculo);
+                veiculos.Add(veiculo);
             }
-            return estacionar;
+            return placa;
         }
 
         private static bool ValidarPlaca(string veiculo)
         {
             if (string.IsNullOrWhiteSpace(veiculo))
-             { 
+            {
                 return false;
-             }
-            if (veiculo.Length > 8) 
-             { 
+            }
+            if (veiculo.Length > 8)
+            {
                 return false;
-             }
+            }
             veiculo = veiculo.Replace("-", "").Trim();
 
             if (char.IsLetter(veiculo, 4))
@@ -62,6 +61,7 @@ namespace DesafioFundamentos.Models
         {
             Console.WriteLine("Digite a placa do veículo para remover:");
 
+            // TODO: Pedir para o usuário digitar a placa e armazenar na variável placa
             // IMPLEMENTADO!
             string placa = "";
             placa = Console.ReadLine();
@@ -71,12 +71,14 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Digite a quantidade de horas que o veículo permaneceu estacionado:");
 
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado
+                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal
                 // IMPLEMENTADO!
-                int horas = 0;
-                horas = Convert.ToInt32(Console.ReadLine());
-                decimal valorTotal = 0;
-                valorTotal = precoInicial + precoPorHora * horas;
 
+                int horas = Convert.ToInt32(Console.ReadLine());
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                // TODO: Remover a placa digitada da lista de veículos
                 // IMPLEMENTADO!
                 veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa.ToUpper()} foi removido e o preço total foi de: {valorTotal:C}");
@@ -93,6 +95,7 @@ namespace DesafioFundamentos.Models
             if (veiculos.Any())
             {
                 Console.WriteLine("Os veículos estacionados são:");
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // IMPLEMENTADO!
                 foreach (var item in veiculos)
                 {
